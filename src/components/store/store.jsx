cdb68dff7f2932558e05648e2090802327fe9e35
Ali-Haider-code1 <AliHaiderAwan1345@gmail.com>
2024-09-19 10:44:59 +0500

    Create zustand Store with complete crud operation for books.

---
+++ src/components/store/store.jsx
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+const useBookStore = create((set) => ({
+    books: [],
+    addBook: (book) => set(state => ({ books: [...state.books, book] })),
+    deleteBook: (id) => set(state => ({ books: state.books.filter(item => item.id !== id) })),
+    updateBook: (id, UpdatedData) => set(state => ({
+        books: state.books.map(book => (book.id === id ? { ...book, ...UpdatedData } : book))
+    })),
+    viewBook: (id) => set(state => {
+        const book = state.books.find(item => item.id === id);
+        return { viewedBook: book || null };
+    }),
+
+    viewedBook: null
+}));
+
+export default useBookStore;
